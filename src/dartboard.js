a006133cecd687277b0ea79fede99172b650e5c6
Alexander Pape <a.pape@paneon.de>
2017-04-19 14:37:40 +0200

    Rendering the board works

--- src/dartboard.js
+++ src/dartboard.js
@@ -2,19 +2,21 @@ const dartValuesMulti = [6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5, 20
 const dartValuesSingle = [3, 19, 7, 16, 8, 11, 14, 9, 12, 5, 20, 1, 18, 4, 13, 6, 10, 15, 2, 17]
 
 export class Dartboard {
-  constructor (canvasElement) {
-    if (!canvasElement.length) {
+  constructor (canvasId) {
+    const canvasElement = window.document.getElementById(canvasId);
+
+    if (!canvasElement) {
       return
     }
     console.log('Initialize Dartboard')
     
-    this.width = (canvasElement.clientWidth)
-    this.height = (canvasElement.clientWidth)
+    this.width = (canvasElement.parentElement.clientWidth)
+    this.height = (canvasElement.parentElement.clientWidth)
     this.radiusBoard = 0.9 * (this.width / 2)
     this.widthTriangle = this.radiusBoard * (170 / 200) * 2 * Math.tan(9 * Math.PI / 180)
     this.heightTriangle = this.radiusBoard * (162 / 200) / Math.cos(9 * Math.PI / 180)
   
-    this.canvas = new fabric.Canvas('canvas', {
+    this.canvas = new fabric.Canvas(canvasId, {
       backgroundColor: 'lightgrey',
       width: this.width,
       height: this.height,
@@ -24,6 +26,7 @@ export class Dartboard {
     
     this.bindEvents()
     this.calculateShapes()
+    this.render()
   }
   
   bindEvents () {
@@ -37,12 +40,33 @@ export class Dartboard {
     })
   }
   
+  render () {
+    for (let x = 0; x < 20; x++) {
+      this.canvas.add(this.singleNumbers[x])
+    }
+  
+    for (let x = 0; x < 20; x++) {
+      this.canvas.add(this.trippleRing[x], this.doubleRing[x])
+    }
+  
+    for (let x = 0; x < 20; x++) {
+      this.canvas.add(this.textNumbers[x])
+    }
+    
+    this.canvas.add(this.singleBull, this.bullsEye, this.noScore)
+  
+    this.canvas.renderAll()
+  }
+  
   calculateShapes () {
     this.doubleRing = []
     this.trippleRing = []
     this.singleNumbers = []
     this.textNumbers = []
     
+    const center = this.width / 2
+    
+    // Triangle Shapes
     for (let x = 0; x < 20; x++) {
       this.singleNumbers[x] = new fabric.Triangle({
         originX: 'center',
@@ -51,12 +75,14 @@ export class Dartboard {
         height: this.heightTriangle,
         fill: (x % 2 === 0) ? 'black' : 'white',
         angle: x * 18,
-        left: this.width / 2,
-        top: this.height / 2,
+        left: center,
+        top: center,
         selectable: false,
         dartValue: dartValuesSingle[x],
       })
     }
+    
+    // Inner Arcs
     for (let x = 0; x < 20; x++) {
       this.doubleRing[x] = new fabric.Circle({
         originX: 'center',
@@ -64,8 +90,8 @@ export class Dartboard {
         radius: this.radiusBoard * (166 / 200),
         angle: -9 + x * 18,
         endAngle: 18 * Math.PI / 180,
-        left: this.width / 2,
-        top: this.height / 2,
+        left: center,
+        top: center,
         fill: 'transparent',
         stroke: (x % 2 === 0) ? 'green' : 'red',
         strokeWidth: this.radiusBoard * (8 / 200),
@@ -73,6 +99,8 @@ export class Dartboard {
         dartValue: dartValuesMulti[x] * 2,
       })
     }
+    
+    // Outer Arcs
     for (let x = 0; x < 20; x++) {
       this.trippleRing[x] = new fabric.Circle({
         originX: 'center',
@@ -80,8 +108,8 @@ export class Dartboard {
         radius: this.radiusBoard * (103 / 200),
         angle: -9 + x * 18,
         endAngle: 18 * Math.PI / 180,
-        left: this.width / 2,
-        top: this.height / 2,
+        left: center,
+        top: center,
         fill: 'transparent',
         stroke: (x % 2 === 0) ? 'green' : 'red',
         strokeWidth: this.radiusBoard * (8 / 200),
@@ -89,67 +117,57 @@ export class Dartboard {
         dartValue: dartValuesMulti[x] * 3,
       })
     }
+    
+    // Text labels
+    for (let x = 0; x < 20; x++) {
+      this.textNumbers[x] = new fabric.Text(dartValuesMulti[x] + '', {
+        originX: 'center',
+        originY: 'center',
+        left: center + this.radiusBoard * (185 / 200) * Math.cos((2 * Math.PI * x) / 20),
+        top: center + this.radiusBoard * (185 / 200) * Math.sin((2 * Math.PI * x) / 20),
+        fontSize: this.radiusBoard * (30 / 200),
+        fill: 'white',
+        angle: 90 + (360 / 20 * x),
+        selectable: false,
+        dartValue: 0,
+      })
+    }
+  
+    this.bullsEye = new fabric.Circle({
+      originX: 'center',
+      originY: 'center',
+      radius: this.radiusBoard * (12.7 / 200),
+      fill: 'red',
+      left: center,
+      top: center,
+      dartValue: 50,
+      selectable: false,
+    })
+    
+    this.singleBull = new fabric.Circle({
+      originX: 'center',
+      originY: 'center',
+      radius: this.radiusBoard * (26.8 / 200),
+      fill: 'green',
+      left: center,
+      top: center,
+      dartValue: 25,
+      selectable: false,
+    })
+    
+    this.noScore = new fabric.Circle({
+      originX: 'center',
+      originY: 'center',
+      radius: this.radiusBoard * (185 / 200),
+      left: center,
+      top: center,
+      fill: 'transparent',
+      stroke: 'black',
+      strokeWidth: this.radiusBoard * (30 / 200),
+      selectable: false,
+      dartValue: 0,
+    })
   }
 }
 
 export default Dartboard
-
-const bullsEye = new fabric.Circle({
-  originX: 'center',
-  originY: 'center',
-  radius: radiusBoard * (12.7 / 200),
-  fill: 'red',
-  left: width / 2,
-  top: height / 2,
-  dartValue: 50,
-  selectable: false,
-})
-const singleBull = new fabric.Circle({
-  originX: 'center',
-  originY: 'center',
-  radius: radiusBoard * (26.8 / 200),
-  fill: 'green',
-  left: width / 2,
-  top: height / 2,
-  dartValue: 25,
-  selectable: false,
-})
-const noScore = new fabric.Circle({
-  originX: 'center',
-  originY: 'center',
-  radius: radiusBoard * (185 / 200),
-  left: width / 2,
-  top: height / 2,
-  fill: 'transparent',
-  stroke: 'black',
-  strokeWidth: radiusBoard * (30 / 200),
-  selectable: false,
-  dartValue: 0,
-})
-for (let x = 0; x < 20; x++) {
-  textNumbers[x] = new fabric.Text(dartValuesMulti[x] + '', {
-    originX: 'center',
-    originY: 'center',
-    left: width / 2 + radiusBoard * (185 / 200) * Math.cos((2 * Math.PI * x) / 20),
-    top: height / 2 + radiusBoard * (185 / 200) * Math.sin((2 * Math.PI * x) / 20),
-    fontSize: radiusBoard * (30 / 200),
-    fill: 'white',
-    angle: 90 + (360 / 20 * x),
-    selectable: false,
-    dartValue: 0,
-  })
-}
-
-for (let x = 0; x < 20; x++) {
-  canvas.add(singleNumbers[x])
-}
-
-for (let x = 0; x < 20; x++) {
-  canvas.add(trippleRing[x], doubleRing[x], singleBull, bullsEye, noScore)
-}
-
-for (let x = 0; x < 20; x++) {
-  canvas.add(textNumbers[x])
-}
-
-canvas.renderAll()
