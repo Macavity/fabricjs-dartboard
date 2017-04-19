1d2647af97aee869aa42147722c2be605ebd0c2f
Alexander Pape <alexander.pape@myposter.de>
2017-04-19 11:15:23 +0200

    Add missing eslint plugins

--- server.js
+++ server.js
@@ -5,19 +5,19 @@
  * see the webpack-dev-server API docs:
  * https://github.com/webpack/docs/wiki/webpack-dev-server#api
  */
-var WebpackDevServer = require('webpack-dev-server');
-var webpack = require('webpack');
-var config = require('./webpack.config.js');
-var path = require('path');
+var WebpackDevServer = require('webpack-dev-server')
+var webpack = require('webpack')
+var config = require('./webpack.config.js')
 
-var compiler = webpack(config);
+var compiler = webpack(config)
 var server = new WebpackDevServer(compiler, {
-    contentBase: 'www',
-    hot: true,
-    filename: 'bundle.js',
-    publicPath: '/',
-    stats: {
-        colors: true
-    }
-});
-server.listen(8080, 'localhost', function() {});
+  contentBase: 'www',
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true
+  }
+})
+
+server.listen(8080, 'localhost', function () {})
